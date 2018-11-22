9ee68645a2bbde995b3eb2e1492769ac264c2fa3
Štěpán Vraný <vrany.stepan@gmail.com>
2018-11-22 12:37:34 +0100

    remove debug

--- vars/PipelineNode.groovy
+++ vars/PipelineNode.groovy
@@ -17,7 +17,6 @@ def call(body) {
       // https://jenkins.io/doc/pipeline/steps/workflow-scm-step/
       stage('Checkout') {
         pipelineStep = "checkout"
-        if(config.debugMode) { echo scm.getUserRemoteConfigs().dump() }
         if (!fileExists('repo')){ new File('repo').mkdir() }
         dir('repo') { checkout scm }
 
