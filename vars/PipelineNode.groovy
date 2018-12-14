e7a2a17999f05a31c5796f68dfe1c8a090b5f899
Stepan Vrany <stepan.vrany@ackee.cz>
2018-12-14 13:47:25 +0100

    test

--- vars/PipelineNode.groovy
+++ vars/PipelineNode.groovy
@@ -175,13 +175,8 @@ def call(body) {
 
       if(config.testConfig) {
         // publish test results
-        step([
-          $class: 'JUnitResultArchiver',
-          allowEmptyResults: true,
-          healthScaleFactor: 10.0,
-          keepLongStdio: true,
-          testResults: 'ci-outputs/mocha/test.xml'
-        ])
+        junit 'ci-outputs/mocha/test.xml'
+
         echo "junit finished. currentBuild.result=${currentBuild.result}"
 
         // publish coverage results
