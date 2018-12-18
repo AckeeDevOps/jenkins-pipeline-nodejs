6c0ba4086394973818b7bd5740db6423bde03e35
Stepan Vrany <stepan.vrany@ackee.cz>
2018-12-18 15:55:59 +0100

    get author inside repo dir

--- vars/getNodeAuthorName.groovy
+++ vars/getNodeAuthorName.groovy
@@ -1,12 +1,14 @@
 def call() {
-  wrap([$class: 'BuildUser']) {
-    try {
-      def userId = "${BUILD_USER_ID}"
-      userId = sh(script: "echo $userId | cut -d@ -f1", returnStdout: true).trim()
-      return userId + ' (manually)'
-    } catch(Exception ex) {
-      def commiter = sh(script: 'git show -s --pretty=%ae | cut -d@ -f1',  returnStdout: true).trim()
-      return commiter + ' (commit author)'
+  dir('repo') {
+    wrap([$class: 'BuildUser']) {
+      try {
+        def userId = "${BUILD_USER_ID}"
+        userId = sh(script: "echo $userId | cut -d@ -f1", returnStdout: true).trim()
+        return userId + ' (manually)'
+      } catch(Exception ex) {
+        def commiter = sh(script: 'git show -s --pretty=%ae | cut -d@ -f1',  returnStdout: true).trim()
+        return commiter + ' (commit author)'
+      }
     }
   }
 }
