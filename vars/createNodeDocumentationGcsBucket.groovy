230c48c65fd3444efa88871a4f072f66aa29825e
Štěpán Vraný <vrany.stepan@gmail.com>
2018-12-21 12:14:29 +0100

    Update createNodeDocumentationGcsBucket.groovy

--- vars/createNodeDocumentationGcsBucket.groovy
+++ vars/createNodeDocumentationGcsBucket.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
-  def storageClass = config.documenation.storageClass ? config.documenation.storageClass : "regional"
-  def location = config.documenation.location ? config.documenation.location : "europe-west3"
+  def storageClass = config.documentation.storageClass ? config.documentation.storageClass : "regional"
+  def location = config.documentation.location ? config.documentation.location : "europe-west3"
 
   def command = "gsutil mb -c ${storageClass} " +
     "-l ${location} " +
