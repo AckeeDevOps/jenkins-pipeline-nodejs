746e059c443fec4e33f1d0bd283f944012b8a412
Štěpán Vraný <vrany.stepan@gmail.com>
2018-12-21 12:24:44 +0100

    Update createNodeDocumentationGcsBucket.groovy

--- vars/createNodeDocumentationGcsBucket.groovy
+++ vars/createNodeDocumentationGcsBucket.groovy
@@ -2,13 +2,15 @@ def call(Map config) {
   def storageClass = config.documentation.storageClass ? config.documentation.storageClass : "regional"
   def location = config.documentation.location ? config.documentation.location : "europe-west3"
 
-  def bucketExists = sh(script: "gsutil ls -p ${config.documentation.gcpProjectId} | grep ${config.documentation.bucketUrl}", returnStatus: true)
+  def bucketExistsCommand = "gsutil ls -p ${config.documentation.gcpProjectId} | grep ${config.documentation.bucketUrl}"
+  def bucketExists = sh(script: bucketExistsCommand, returnStatus: true)
   
   if(bucketExists != 0){
     def command = "gsutil mb -c ${storageClass} " +
       "-l ${location} " +
       "-p ${config.documentation.gcpProjectId} " +
       "${config.documentation.bucketUrl}"
+    
     sh(script: command)
   } else {
     echo("Bucket ${config.documentation.bucketUrl} (project: ${config.documentation.gcpProjectId}) already exists.")
