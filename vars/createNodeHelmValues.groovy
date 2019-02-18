873cf97bedf8816839c184cdc94d47854c1d604e
Štěpán Vraný <stepan.vrany@ackee.cz>
2019-02-18 20:03:29 +0100

    Create createNodeHelmValues.groovy

---
+++ vars/createNodeHelmValues.groovy
@@ -0,0 +1,31 @@
+import groovy.json.*
+
+def call(Map config, Map secrets) {
+
+  def values = config.helmValues
+
+  //general section
+  values.general = [:]
+  values.general.appName = config.appName
+  values.general.projectName = config.projectFriendlyName
+  values.general.appRole = config.appRole
+  values.general.appTier = config.appTier
+  values.general.envName = config.envDetails.friendlyEnvName
+  values.general.imageTag = config.dockerImageTag
+  values.general.gcpProjectId = config.envDetails.gcpProjectId
+
+  // build specific info
+  values.general.meta = [:]
+  values.general.meta.builtBy = config.startedBy
+  values.general.meta.buildNumber = config.buildNumber
+  values.general.meta.branch = config.branch
+  values.general.meta.repositoryUrl = config.repositoryUrl
+
+  //secretsSection
+  if(config.secretsInjection) {
+    values.secrets = [:]
+    values.secrets.data = secrets
+  }
+
+  return JsonOutput.toJson(values)
+}
