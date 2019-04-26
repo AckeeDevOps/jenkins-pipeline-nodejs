6663b091db93de824435a75a39fa3c5b0eb5b995
Štěpán Vraný <vrany.stepan@gmail.com>
2019-04-26 15:33:01 +0200

    Update createNodeSecretsManifest.groovy

--- vars/createNodeSecretsManifest.groovy
+++ vars/createNodeSecretsManifest.groovy
@@ -5,8 +5,7 @@ def call(Map config) {
     withCredentials([string(credentialsId: config.envDetails.vaultTokenSecretId, variable: 'VAULT_TOKEN')]) {
       env.VAULTIER_VAULT_ADDR = config.envDetails.vaultAddr
       env.VAULTIER_VAULT_TOKEN = env.VAULT_TOKEN
-      env.VAULTIER_BRANCH = config.branch
-      env.VAULTIER_RUN_CAUSE = "delivery"
+      env.VAULTIER_ENVIRONMENT = config.envDetails.friendlyEnvName
       env.VAULTIER_OUTPUT_FORMAT = "helm"
       env.VAULTIER_SECRET_SPECS_PATH = config.envDetails.secretSpecsPath ?: "${config.workspace}/repo/secrets.yaml"
       env.VAULTIER_SECRET_OUTPUT_PATH = "${config.workspace}/secrets-deployment.json"
